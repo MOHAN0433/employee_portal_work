8796233f6c076f916ab5d453246ca1ff03b5f5f8
Mohan Patil <mohan.patil@hyniva.com>
2023-09-27 10:45:57 +0530

    validations new

--- api.js
+++ api.js
@@ -3,57 +3,53 @@ const { marshall } = require("@aws-sdk/util-dynamodb");
 
 const db = new DynamoDBClient({ region: "ap-south-1" });
 
-exports.createPost = async (event) => {
-  const requestBody = JSON.parse(event.body);
-  const postId = uuid.v4();
-
-  const params = {
-    TableName: 'employeeTable',
-    Item: {
-      postId,
-    },
-  };
-
-  // Define a list of fields that can be added
-  const fieldsToAdd = [
-    'jobTitle',
-    'totalExperience',
-    'employmentStartDateInHyniva',
-    'supervisorName',
-    'projectName',
-    'clientName',
-    'plannedJoiningDateInTheProject',
-    'plannedEndDateInTheProject',
-    'actualJoiningDateInTheProject',
-    'actualEndDateInTheProject',
-    'location',
-    'billability',
-    'remarksOnBillability',
-    'laptopSpecification',
-    'asset1',
-    'asset2'
-  ];
-
-  // Add fields from the request body if they exist
-  fieldsToAdd.forEach((field) => {
-    //if (requestBody[field]) {
-      params.Item[field] = requestBody[field] || null;
-    //}
-  });
-
+const createPost = async (event) => {
+  const response = { statusCode: 200 };
   try {
-    await dynamoDB.put(params).promise();
+    const body = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ postId }),
+    // Check for required fields
+    // if (!body.postId || !body.firstName || !body.lastName || !body.email) {
+    //   throw new Error('Required fields are missing.');
+    // }
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall({
+        postId: body.postId,
+        jobTitle: body.jobTitle || null,
+        totalExperience: body.totalExperience || null,
+        employmentStartDateInHyniva: body.employmentStartDateInHyniva || null,
+        supervisorName: body.supervisorName || null,
+        projectName: body.projectName || null,
+        clientName: body.clientName || null,
+        plannedJoiningDateInTheProject: body.plannedJoiningDateInTheProject || null,
+        plannedEndDateInTheProject: body.plannedEndDateInTheProject || null,
+        actualJoiningDateInTheProject: body.actualJoiningDateInTheProject || null,
+        actualEndDateInTheProject: body.actualEndDateInTheProject || null,
+        location: body.location || null,
+        billability: body.billability || null,
+        remarksOnBillability: body.remarksOnBillability || null,
+        laptopSpecification: body.laptopSpecification || null,
+        asset1: body.asset1 || null,
+        asset2: body.asset2 || null,
+      }),
     };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Could not create the post' }),
-    };
+
+    await db.send(new PutItemCommand(params));
+    response.body = JSON.stringify({
+      message: 'Successfully created post.',
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: 'Failed to create post.',
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
   }
+  return response;
 };
 
 module.exports = {
